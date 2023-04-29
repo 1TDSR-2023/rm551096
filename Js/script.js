c2eac445591a4e6a66f7fe854a11a54d17d88cd3
ruanJS <rm551096@fiap.com.br>
2023-04-28 22:59:24 -0300

    Introduzindo Js

---
+++ Js/script.js
@@ -0,0 +1,13 @@
+console.log("Alo Mundo!!");
+
+var nome1 = "Petroni";
+//let nome2 = "Derick";
+//const nome3 = "Ruan";
+
+nome
+
+if(nome1 == "") {
+    var nome1 = "Geovana";
+}
+
+console.log(nome1);
