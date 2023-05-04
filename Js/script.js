bf9cac799a1340838cedc816c6f28cfc91e2cdb3
CavichioliFIAP <rm551100@fiap.com.br>
2023-05-03 21:46:38 -0300

    Finalizando diferenças entre var, let, cons, hoisting

--- Js/script.js
+++ Js/script.js
@@ -1,13 +1,20 @@
 console.log("Alo Mundo!!");
 
-var nome1 = "Petroni";
+//var nome1 = "Petroni";
 //let nome2 = "Derick";
 //const nome3 = "Ruan";
 
-nome
+//nome
 
-if(nome1 == "") {
-    var nome1 = "Geovana";
-}
+//if(nome1 == "") {
+//var nome1 = "Geovana";
+   
+//if(nome1 != ""){
+    //let nome1 = "Geovana"
+//}
+//}
 
-console.log(nome1);
+//console.log(nome1);
+
+
+
