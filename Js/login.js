ab8435a9aabd4bb1de19e1ec575be8d54513ae50
ruanJS <rm551096@fiap.com.br>
2023-05-17 22:20:30 -0300

    login index e js criados

---
+++ Js/login.js
@@ -0,0 +1,35 @@
+
+
+const inputEmail = document.querySelector("#idEmail");
+
+
+inputEmail.addEventListener("click", ()=>   {
+
+    const lblEmail = document.querySelector("label[for='idEmail']")
+
+    if(inputEmail.value.length < 5){
+        inputEmail.setAttribute("style","outline-color:#ff0000;");
+        lblEmail.setAttribute("style"," color:#ff0000;");
+    }else{
+        inputEmail.setAttribute("style","outline-color:#00ff00;");
+        lblEmail.setAttribute("style","color:#00ff00;");
+    }
+    console.log(inputEmail.Value.length)
+
+   // inputEmail.setAttribute("style","outline-color:#ff0000;");
+});
+
+const eyePass  = document.querySelector(".fa");
+
+eyePass.addEventListener("click", ()=>{
+
+    const inputSenha = document.querySelector("#idPass");
+
+    if(inputSenha.getAttribute("type") == "password"){
+        inputSenha.setAttribute("type","text");
+        eyePass.setAttribute("class","fa fa-eye-slash")
+    }else{
+        inputSenha.setAttribute("type","password");
+        eyePass.setAttribute("class","fa fa-eye")
+    }
+});
